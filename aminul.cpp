de4097d741f92d70c64ec93a16ef0b7ba69dd170
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-13 18:46:37 +0600

    first commit

---
+++ aminul.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define endl '\n'
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(auto &x : v)
+    cin>>x;
+    //precomputation
+    ll q;
+    cin>>q;
+    while(q--)
+    {
+        ll num;
+        cin>>num;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     solve();
+    // }
+    solve();
+    return 0;
+}
