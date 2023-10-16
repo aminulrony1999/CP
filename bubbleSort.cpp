65fbd379b3403a4a472743c9ea421249b8cc0747
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-16 20:10:33 +0600

    bubble sort technique implemented

---
+++ bubbleSort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define endl '\n'
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll> v(n,0);
+    for(auto &x : v)
+    cin>>x;
+    for(ll i = n - 1; i > 0; i--)
+    for(ll j = 0; j < i; ++j)
+    {
+        if(v[j] > v[j + 1])
+        {
+            swap(v[j],v[j + 1]);
+        }
+    }
+    cout<<"After Using Bubble sort technique, the sorted elements are : ";
+    for(auto &x : v)
+    cout<<x<<" ";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
