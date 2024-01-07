700c00f77c5a3df543b4c31f913f403255db2450
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-07 21:00:46 +0530

    char hasing using map

---
+++ charHashMapping.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define endl '\n'
+void solve()
+{
+    string s;
+    cin>>s;
+    map<char,ll> m;
+    for(ll i = 0; i < s.size(); ++i)
+    m[s[i]]++;
+    ll q;
+    cin>>q;
+    while(q--)
+    {
+        char ch;
+        cin>>ch;
+        cout<<m[ch]<<endl;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
