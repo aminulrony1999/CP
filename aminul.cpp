9602dd96380968ad53480130bfa60c19cad698a0
aminulrony1999 <aminulrony1999@gmail.com>
2024-07-06 23:09:22 +0600

    singly linked list

--- aminul.cpp
+++ aminul.cpp
@@ -1,28 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 void merge(vector<int> &arr, int low, int mid, int high)
 {
-
+    vector<int> temp;
+    int leftPointer = low;
+    int rightPointer = mid + 1;
+    while (leftPointer <= mid && rightPointer <= high)
+    {
+        // if (arr[leftPointer] <= arr[rightPointer])
+        if (arr[leftPointer] >= arr[rightPointer])
+        {
+            temp.push_back(arr[leftPointer]);
+            leftPointer++;
+        }
+        else
+        {
+            temp.push_back(arr[rightPointer]);
+            rightPointer++;
+        }
+    }
+    while (leftPointer <= mid)
+    {
+        temp.push_back(arr[leftPointer]);
+        leftPointer++;
+    }
+    while (rightPointer <= high)
+    {
+        temp.push_back(arr[rightPointer]);
+        rightPointer++;
+    }
+    for (int i = low; i <= high; i++)
+        arr[i] = temp[i - low];
 }
 void mergeSort(vector<int> &arr, int low, int high)
 {
-    if(low >= high)
-    return;
-    int mid = (low + high) / 2;
+    if (low >= high)
+        return;
+    int mid = low + (high - low) / 2;
     mergeSort(arr, low, mid);
-    mergeSort(arr,mid+1,high);
+    mergeSort(arr, mid + 1, high);
     merge(arr, low, mid, high);
 }
 int main()
 {
     int n;
-    cin>>n;
-    vector<int> arr(n);
-    for(int i = 0; i < n; ++i)
-    cin>>arr[i];
-    mergeSort(arr,0,n-1);
-    cout<<"After implementing mergesort, the elements of the array are : ";
-    for(int i = 0; i < n; ++i)
-    cout<<arr[i]<<" ";
+    cin >> n;
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    mergeSort(arr, 0, n - 1);
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
     return 0;
 }
